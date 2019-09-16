3334e946007e63f543944ae6487e90f7cd3fad64
Nick Trombley <trombley.nick@gmail.com>
2019-09-16 22:59:10 +0900

    Add blog functionality after starter repo

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,47 @@
+// Most of this taken from:
+// https://github.com/11ty/eleventy-base-blog/blob/bbad06deaecd101a5e38ead1e59ad5087eccaf7a/.eleventy.js
+
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+
 module.exports = function(eleventyConfig) {
+	/* Eleventy behavior */
+	eleventyConfig.setDataDeepMerge(true);
+	eleventyConfig.addPlugin(pluginRss);
+
+	/* Markdown plugins */
+	let markdownIt = require('markdown-it');
+	let markdownItAnchor = require('markdown-it-anchor');
+	let markdownOpts = {
+		html: true,
+		breaks: true,
+		linkify: true
+	};
+	let anchorOpts = {
+		permalink: true,
+		permalinkClass: 'heading-link',
+		permalinkSymbol: '⚓',
+		permalinkSpace: false
+	};
+
+	eleventyConfig.setLibrary(
+		'md',
+		markdownIt(markdownOpts).use(markdownItAnchor, anchorOpts)
+	);
+
+	/* Static files (CSS, images, etc.) */
 	eleventyConfig.addPassthroughCopy({ static: '.' });
 
 	return {
+		templateFormats: ['html', 'liquid', 'md', 'njk'],
+		pathPrefix: '/',
+		markdownTemplateEngine: 'liquid',
+		htmlTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+		passthroughFileCopy: true,
 		dir: {
 			input: 'src',
+			includes: '_includes',
+			data: '_data',
 			output: '_site'
 		}
 	};
---
+++ src/posts/beginning.md
@@ -0,0 +1,9 @@
+---
+title: Testing
+description: Some description here
+date: 2019-09-16
+tags:
+  - long-form
+---
+
+# Hello, world!
